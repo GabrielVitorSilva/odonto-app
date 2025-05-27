fd0b7baa1766eeb81ecc7fbf5b320e72d6eb9765
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-05-26 23:10:37 -0300

    Feat: Tela de login

---
+++ src/app/Login.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
+
+export default function LoginScreen() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      className="flex-1 bg-white  pt-10"
+    >
+      <Text className="text-4xl font-bold text-center mb-32">Entrar</Text>
+
+      <View className="space-y-4 mx-4">
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          className="bg-gray-100 p-4 rounded-md text-base mb-4"
+          placeholderTextColor="#999"
+        />
+
+        <View className="relative">
+          <TextInput
+            placeholder="Senha"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={!passwordVisible}
+            className="bg-gray-100 p-4 rounded-md text-base pr-20"
+            placeholderTextColor="#999"
+          />
+          <TouchableOpacity
+            onPress={() => setPasswordVisible(!passwordVisible)}
+            className="absolute right-4 top-4"
+          >
+            <Text className="text-sky-500 font-medium">Visualizar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <TouchableOpacity className="bg-sky-500 py-4 rounded-full mt-32">
+        <Text className="text-white text-center font-semibold">Entrar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className="mt-4">
+        <Text className="text-sky-500 text-center font-medium">Esqueceu a senha?</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
+}
--- src/routes/StackRoutes.tsx
+++ src/routes/StackRoutes.tsx
@@ -1,5 +1,4 @@
-import Teste1 from "@/app/teste";
-import Teste2 from "@/app/teste2";
+import Login from "@/app/Login";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
@@ -7,13 +6,11 @@ const Stack = createNativeStackNavigator();
 export function StackRoutes() {
   return (
     <Stack.Navigator
-      initialRouteName="Teste1"
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Teste1" component={Teste1} />
-      <Stack.Screen name="Teste2" component={Teste2} />
+      <Stack.Screen name="Teste1" component={Login} />
     </Stack.Navigator>
   );
 }

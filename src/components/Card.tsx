eda51187bef3597b947a67385c47cd515ebbc9f3
oliveira-marcus <marcusvopinto@outlook.com>
2025-05-28 19:34:55 -0300

    refactor: Refatorando o componente Card para ficar mais dinâmico e atualizando a tela HomeClient

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,12 +2,25 @@ import { View, Text, Image } from "react-native";
 
 type CardProps = {
   name: string;
-  description: string;
+  description?: string;
+  patientName?: string;
+  professionalName?: string;
   date: string;
+  hour: string;
   status: string;
+  type: "Admin" | "Client" | "Professional";
 };
 
-export default function Card({ name, description, date, status }: CardProps) {
+export default function Card({
+  name,
+  description,
+  patientName,
+  professionalName,
+  date,
+  hour,
+  status,
+  type,
+}: CardProps) {
   const statusStyle = {
     Confirmado: {
       bg: "bg-[#badec4]",
@@ -26,6 +39,32 @@ export default function Card({ name, description, date, status }: CardProps) {
     text: "text-gray-700",
   };
 
+  function renderUpperText(): string | null {
+    switch (type) {
+      case "Admin":
+        return `Paciente: ${patientName}`;
+
+      case "Client":
+        return "Procedimentos";
+
+      case "Professional":
+        return "Cliente: ";
+    }
+  }
+
+  function renderLowerText(): string {
+    switch (type) {
+      case "Admin":
+        return `Profissional: ${professionalName}`;
+
+      case "Client":
+        return `${description}`;
+
+      case "Professional":
+        return `${patientName}`;
+    }
+  }
+
   return (
     <View className="flex-row gap-4 mb-5">
       <Image
@@ -35,17 +74,19 @@ export default function Card({ name, description, date, status }: CardProps) {
       <View className="flex-1">
         <View className="flex-row justify-between">
           <Text className="font-semibold text-lg">{name}</Text>
-          <Text className="text-[#BDBDBD]">{date}</Text>
+          <Text className="text-[#BDBDBD]">
+            {date} - {hour}
+          </Text>
         </View>
 
         <View className="flex-row justify-between mb-2 items-center">
-          <Text>Procedimentos: </Text>
+          <Text>{renderUpperText()}</Text>
           <View className={`rounded-[10px] px-1 py-[2px] ${statusStyle.bg}`}>
             <Text className={`font-bold ${statusStyle.text}`}>{status}</Text>
           </View>
         </View>
 
-        <Text>{description}</Text>
+        <Text>{renderLowerText()}</Text>
       </View>
     </View>
   );

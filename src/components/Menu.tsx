88c2953c826fcb32716701074139929fff993352
oliveira-marcus <marcusvopinto@outlook.com>
2025-06-06 12:59:12 -0300

    feat: Alteração no componente Menu para que seja possível scrollar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 type IoniconsName = React.ComponentProps<typeof Ionicons>["name"];
 
@@ -16,21 +16,24 @@ type MenuProps = {
 export function Menu({ items }: MenuProps) {
   return (
     <>
-      <View className="p-5">
+      <View className="flex-1 p-5">
         <Text className="text-app-blue mb-5 text-center text-lg font-semibold pb-2 border-b-hairline border-b-gray-300">
           Menu
         </Text>
-
-        {items.map((item, index) => (
-          <TouchableOpacity
-            key={index}
-            className="flex-row gap-6 items-center mb-3"
-            onPress={item.handlePress}
-          >
-            <Ionicons name={item.icon} size={48} color="black" />
-            <Text className="font-bold">{item.title}</Text>
-          </TouchableOpacity>
-        ))}
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item.toString()}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              key={index}
+              className="flex-row gap-6 items-center mb-3"
+              onPress={item.handlePress}
+            >
+              <Ionicons name={item.icon} size={48} color="black" />
+              <Text className="font-bold">{item.title}</Text>
+            </TouchableOpacity>
+          )}
+        />
       </View>
     </>
   );

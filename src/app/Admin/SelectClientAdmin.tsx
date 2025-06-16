201b79527002950c5c1ac1d73357150da1a63f4f
caioliboreiro <deganaqualy@gmail.com>
2025-06-16 14:27:03 -0300

    shorty: Consertando Loading ta tela de Selecionar Cliente de Admin

--- src/app/Admin/SelectClientAdmin.tsx
+++ src/app/Admin/SelectClientAdmin.tsx
@@ -56,7 +56,7 @@ export default function SelectClientAdmin() {
         </Text>
 
         <View className="flex-1">
-          {Loading ? (
+          {loading ? (
             <Loading />
           ) : (
             <SingleSelectList

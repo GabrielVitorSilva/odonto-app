a6f8328c9de5a396c1a66aa6c2b23e9ca4fc3798
caioliboreiro <deganaqualy@gmail.com>
2025-07-01 11:28:03 -0300

    shorty: Adicionando margem para os botões na tela de Consulta

--- src/app/Professional/ConsultationPageProf.tsx
+++ src/app/Professional/ConsultationPageProf.tsx
@@ -90,7 +90,7 @@ export default function ConsultationPageProf() {
           </View>
         </View>
 
-        <View>
+        <View className="mb-4">
           {statusState === "SCHEDULED" && (
             <>
               <Button
